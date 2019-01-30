e6a12651f0bb5a2d4abb014175874dcc2ee43c38
Vladymir <vladymirsheyko@gmail.com>
2019-01-30 23:46:21 +0200

    added some changes in Factory pattern

--- src/basepatterns/creational/factory/ClientAppRunner.java
+++ src/basepatterns/creational/factory/ClientAppRunner.java
@@ -2,12 +2,21 @@ package basepatterns.creational.factory;
 
 public class ClientAppRunner {
     public static void main(String[] args) {
-//        TeacherFactory teacherFactory = new MathTeacherFactory();
-        TeacherFactory teacherFactory1 = createTeacherFactoryBySpecialty("math");
-        TeacherFactory teacherFactory2 = createTeacherFactoryBySpecialty("chemistry");
+        //it's just Factory pattern without using static method createTeacherFactoryBySpecialty:
+        // for creating another type of teacher we have to change
+        TeacherFactory teacherFactory = new MathTeacherFactory(); // only this line of code new SomeTeacherFactory
+        Teacher teacher = teacherFactory.createTeacher();
+        teacher.teach();
+
+        // it's Factory pattern using static method createTeacherFactoryBySpecialty
+        //for creating another type of teacher we have to change only argument
+        // in createTeacherFactoryBySpecialty method
+        TeacherFactory teacherFactory1 = createTeacherFactoryBySpecialty("math"); // here!
+        // this two lines of code stay without changing
         Teacher teacher1 = teacherFactory1.createTeacher();
         teacher1.teach();
 
+        TeacherFactory teacherFactory2 = createTeacherFactoryBySpecialty("chemistry");
         Teacher teacher2 = teacherFactory2.createTeacher();
         teacher2.teach();
     }
@@ -23,3 +32,8 @@ public class ClientAppRunner {
         }
     }
 }
+
+/*  Math teacher teaches math
+    Math teacher teaches math
+    Chemistry teacher teaches chemistry
+*/
---
+++ src/basepatterns/creational/factory/README.md
@@ -0,0 +1,25 @@
+In **Factory pattern**, we create object `without exposing the creation logic to the client`
+ and refer to newly created object using _a common interface_.
+ In our example we create factory for creating teacher.
+ * ***Step 1:*** create the interface Teacher with method `void teach();`
+  * ***Step 2:*** create concrete classes implementing the same interface:
+    * MathTeacher
+    * ChemistryTeacher
+  * ***Step 3:*** create the interface TeacherFactory with method `Teacher createTeacher();
+  * ***Step 4:*** create concrete classes implementing the same interface:
+    * MathTeacherFactory, with _returns new MathTeacher();_
+    * ChemistryTeacherFactory, with _returns new ChemistryTeacher;_
+  * ***Step 5:*** in ClientAppRunner create the concrete factory:
+    * `TeacherFactory teacherFactory = new` `SomeTeacherFactory();` // here Some = Math or Chemistry
+
+    * `Teacher teacher = teacherFactory.createTeacher();`
+    * `teacher.teach();`
+    
+  * ***Step 6:*** for improving this line of code: `TeacherFactory teacherFactory = new` `SomeTeacherFactory();`
+  create static method `static private TeacherFactory createTeacherFactoryBySpecialty(String specialty) ` and then 
+  change this line in the following way:
+    `* TeacherFactory teacherFactory1 = createTeacherFactoryBySpecialty("some spesialty");`// here some spesialty
+      = "chemistry" or "math" 
+ 
+ 
+ 
